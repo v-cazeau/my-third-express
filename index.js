8e93047e9b065400edd2c77c411191383cca0621
Véronie R. Cazeau <veronie.cazeau@gmail.com>
2023-04-03 16:22:29 -0400

    FIRST commit

---
+++ index.js
@@ -0,0 +1,16 @@
+import express from 'express'; 
+
+const PORT = 3050; 
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('I am index'); 
+}); 
+
+app.get('/hoody', (req,res) => {
+    res.send('I am a hoody.');
+}); 
+
+app.listen(PORT, () => {
+    console.log(`Listening on http://localhost:${PORT}`)
+}); 
